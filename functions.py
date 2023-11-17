aee34a2b1f74c94df3763cacfa77a86693425034
PierreMo <pierremouries05@gmail.com>
2023-11-17 08:51:51 +0100

    First commit, test, with job already done without git

---
+++ functions.py
@@ -0,0 +1,117 @@
+import os
+
+def list_of_files(directory : str, extension : str) -> list:
+    '''Function which takes a directory and a type of file (extension) as parameters.
+        It returns the name of all files which correspond to the extension in the given directory. '''
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+def finding_names(files_names : list) -> list:
+    ''' Take a list of files names such as Nomination_[president's name][number].txt
+    '''
+    names = set()
+    for file in files_names:
+        # taking the string with Nomination_ and .txt
+        name= file[11:-4]
+        # removing the number at end if there is one
+        if 48 <= ord(name[-1]) <= 57:
+            name = name[:-1]
+        names.add(name)
+    return list(names)
+
+def print_list(L : list):
+    '''
+    Function which display a list properly (from Lab 5)
+    '''
+    for e in L[:-1]:
+        print(e, end=', ')
+    print(L[-1])
+
+def new_folder(name):
+    '''
+    Creating a new folder, do nothing if it already exists (I saw how to catch an error last year)
+    '''
+    try :
+        os.mkdir('name')
+    except FileExistsError:
+       pass
+
+def file_in_lowercase(file :str) -> str:
+    '''
+    Function which take as input a full file name.
+    Return the content of the file as a string with punctuation sign and with all letters in lowercase
+    '''
+    with open(file, 'r') as f:
+        content = f.readlines()
+    #cleaning
+    content_str = ''
+    for line in content:
+        #removing \n
+        line = line[:-1]
+        #cleaning character by character for each case
+        for char in line:
+            # if upercase -> lowercase
+            if 65 <= ord(char) <= 90:
+                content_str += chr(ord(char) +32)
+            else:
+                content_str += char
+    print(content_str)
+    return content_str
+
+def write_in_file(file:str, content : str):
+    with open(file, 'w') as f:
+        f.write(content)
+
+
+def file_cleaning(file :str):
+    '''
+    Take a file name, the file has to be a one line file (only the first line in treated)
+    Return the content of the file without punctuation, only words separated by spaces
+    '''
+    with open(file, 'r') as f:
+        line = f.readline()
+
+    word = ''
+    content = []
+    for character in line:
+        if character in ["'", ' ', '-', '?', '.', ',', ';', ':', '/', '(', ')', '!', '%', '"']:
+            content.append(word)
+            word = ''
+        else:
+            word+=character
+
+    print(content)
+
+    content_string = ''
+    for word in content:
+        if word != '':
+            content_string += word + ' '
+
+    return content_string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from functions import *
+
+## PART I ##
+
+# Taking the name of the files
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+
+# Finding the name of the different presidents from the names of the files and display them
+presidents_names = finding_names(files_names)
+print_list(presidents_names)
+
+# Creating a new folder
+new_folder('cleaned')
+
+# Putting in lowercase and one line each file from the 'speeches' folder and storing them in the 'cleaned' repertory
+for speech_file in files_names:
+    destination = "./speeches/" + speech_file
+    text_lowercase = file_in_lowercase(destination)
+    destination = "./cleaned/" + speech_file
+    write_in_file(destination, text_lowercase)
+
+# Cleaning the text: removing punctuation, double spaces, rewriting the files in 'cleaned'
+for speech_file in files_names:
+    destination = "./cleaned/" + speech_file
+    text_cleaned = file_cleaning(destination)
+    write_in_file(destination, text_cleaned)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
